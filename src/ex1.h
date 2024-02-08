d5a82c2613469020bcab8035dc2734149fb5e26e
heavydemon21 <nielsstunnebrink1@gmail.com>
2024-02-08 11:25:44 +0100

    exercise 1 n 2

---
+++ src/ex1.h
@@ -0,0 +1,7 @@
+
+
+
+void say_odd(int n);
+void say_even(int n);
+void say(int n);
+
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+
+#include "ex1.h"
+#include "ex2.h"
+
+
+int main(int argc, char **argv) {
+
+  Test* t = new Test();
+  return 0;
+
+
+
+}
